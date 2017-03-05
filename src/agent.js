876402b20aa0e4a03477f2e0e2cf3a6742413f40
js-palace <edeveloper4@gmail.com>
2017-03-04 21:17:52 -0500

    Register component is added, register function is completed

--- src/agent.js
+++ src/agent.js
@@ -27,10 +27,12 @@ const Articles = {
 }
 
 const Auth = {
+  current: () =>
+    requests.get('/user'),
   login: (email, password) =>
     requests.post('/users/login', { user: { email, password } }),
-  current: () =>
-    requests.get('/user')
+  register: (username, email, password) =>
+    requests.post('/users', { user: { username, email, password } })
 }
 
 export default {
